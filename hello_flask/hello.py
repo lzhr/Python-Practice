e50aa855465910cb494385cb0dadcdec438581a5
lzhrsmile <lzhrsmile@gmail.com>
2016-06-19 23:29:34 +0800

    templates/base.html

--- hello_flask/hello.py
+++ hello_flask/hello.py
@@ -9,20 +9,9 @@ def index():
 #@app.route('/hello')
 #def hello_world():
 #    return 'Hello World!'
-
 @app.route('/hello/<name>')
 def hello_world(name):
     return 'Hello World!<br> %s' % name
-
-@app.route('/user/<name>')
-def sayHello(name):
-    if name == 'baidu':
-        return redirect('http://www.baidu.com')
-    elif name == 'NO':
-        return abort(404)
-
-    return render_template('user.html',user=name)
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug = True)
--- hello_flask/user.py
+++ hello_flask/user.py
@@ -1,16 +1,19 @@
-
 from flask import Flask,render_template,abort,redirect
 
 app = Flask(__name__)
 
 @app.route('/user/<name>')
-def sayHello(name):
-    if name == 'baidu':
-        return redirect('http://www.baidu.com')
-    elif name == 'NO':
-        return abort(404)
-
+def user(name):
+    #if name == 'baidu':
+    #    return redirect('http://www.baidu.com')
+    #elif name == 'NO':
+    #    return abort(404)
     return render_template('user.html',user=name)
 
+@app.route('/page')
+def page():
+    return render_template('page.html')
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug = True)
+    #app.run(host='0.0.0.0',debug = True)
+     app.run(debug=True)
