d7ba554837116e5c4a49b1c0632e6ba6bfad2ec7
lzhrsmile <lzhrsmile@gmail.com>
2016-06-28 10:18:54 +0800

    import bootstrap

--- hello_flask/user.py
+++ hello_flask/user.py
@@ -1,6 +1,10 @@
 from flask import Flask,render_template,abort,redirect
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/user/<name>')
 def user(name):
