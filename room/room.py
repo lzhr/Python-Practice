c72a3447661e85fe90849805f257170a20b14383
lzhrsmile <lzhrsmile@gmail.com>
2017-06-16 02:43:54 +0800

    favicon.ico

--- room/room.py
+++ room/room.py
@@ -1,43 +1,83 @@
 import types
 import urllib.request, urllib.error, urllib.parse
 import json
-import gzip 
-import struct
+import threading
+import time
+import sys
+import getopt
+# import io
+# sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf-8')
 
-def registerUrl():
+def registerUrl(room_id):
 	try:
-		url ="http://open.douyucdn.cn/api/RoomApi/Room/220149"
+		url ="http://open.douyucdn.cn/api/RoomApi/Room/"+room_id
 		res220149 = urllib.request.urlopen(url).read()
-		print(type(res220149))
-		#res = gzip.decompress(res220149)
-		#return res
+		#print(type(res220149))
 		return res220149
 	except Exception as e:
 		print(e)
 		
 def jsonFile(fileData):
-	file = open("D:\WorkSpace\Python\json.txt","w")
-	file.write(fileData)
+	file = open("Room.json","w")
+	file.write(fileData.decode('utf-8'))
 	file.close()
 
-def praserJsonFile(jsonData):
-	value = json.loads(jsonData)
-	rootlist = list(value.keys())
-	print(rootlist)
-	for rootkey in rootlist:
-		print(rootkey)
-	subvalue = value[rootkey]
-	print(subvalue)
-	for subkey in subvalue:
-		print(subkey,subvalue[subkey])
-		
-def alert(message):
-	json.loads(res)
+def readFile():
+	file = open("Room.json",encoding='utf-8')
+	try:
+		jsonf = file.read()
+		return jsonf
+	finally:
+		file.close()
 
+def praserJsonFile(jsonData,count):
+	value = json.loads(jsonData)
+	#print(type(value))
+	#print(type(value['data']['room_status']))
+	da = value['data']
+	nickname = da['owner_name']
+	status = da['room_status']
+	start_time = da['start_time']
+	online = da['online']
+	#room_name = da['room_name']
+	print('查询次数：',count)
+	
+	if status == '1' :
+		print(nickname,'正在直播ing')
+		#print('房间名称：',room_name)
+		print('人气值：',online)
+	else:
+		print('主播 ',nickname,'不在直播')
+		#print('房间名称：',room_name)
+		print('上次开播时间：',start_time)
+	count += 1
+	return count
+		
+#def alert(message):
+	#json.loads(res)
+
+def fun_timer(im,room_id):
+	jsonFile(registerUrl(room_id))
+	jsonf = readFile()
+	i = praserJsonFile(jsonf,im)
+	print('本地时间:',time.strftime("%Y-%m-%d %X"))
+	global timer
+	timer = threading.Timer(10,fun_timer,[i,room_id])
+	timer.start()
+
+	
 if __name__ == "__main__":
-	res = registerUrl()
-	print(res)
-	
-	praserJsonFile(data)
-	
-	
+	room_id = '220149'
+	print('输入的房间ID：')
+	if len(sys.argv) == 2 :
+		print(sys.argv[1])
+		room_id = sys.argv[1]
+	else :
+		print(room_id)
+	# inputid = ''
+	# try:
+		# opts,args = getopt.getopt(argv,"")
+	i = 1
+	timer = threading.Timer(1,fun_timer,[i,room_id])
+	timer.start()
+ 
